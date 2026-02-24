df089d44d9a9cf2e29671cf661079a644b0681c3
1111-ssss <gurskievgeni@gmail.com>
2026-02-24 09:33:58 +0300

    feat(versionsPage): add base intefrace

--- admin/Features/Versions/VersionsViewModel.cs
+++ admin/Features/Versions/VersionsViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using admin.Core.Abstractions;
 using admin.Core.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.Logging;
 
 namespace admin.Features.Versions;
 
 public partial class VersionsViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    private List<string> _branches = new() { "Stable", "Dev" };
+
     private readonly IApiService _apiService;
     private readonly ICurrentUserService _currentUserService;
     private readonly ILogger<VersionsViewModel> _logger;
