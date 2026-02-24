7d0792787219c018872518ece431aeab6740e8f0
1111-ssss <gurskievgeni@gmail.com>
2026-02-24 08:55:17 +0300

    fix(profile): remove profile page

--- admin/AppBootstrapper/Extensions/VMsAndPagesExtensions.cs
+++ admin/AppBootstrapper/Extensions/VMsAndPagesExtensions.cs
@@ -1,7 +1,6 @@
 ﻿using admin.Features.Auth.Pages;
 using admin.Features.Home;
 using admin.Features.Metrics;
-using admin.Features.Profile;
 using admin.Features.Settings;
 using admin.Features.Users;
 using admin.Features.Versions;
