a3edf5cbf66dc482a3d40d5d788c0652863c1ae1
1111-ssss <gurskievgeni@gmail.com>
2026-02-28 11:07:00 +0300

    feat(versionsPage): add fetch method

--- admin/Features/Versions/VersionsViewModel.cs
+++ admin/Features/Versions/VersionsViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using admin.Core.Abstractions;
+using admin.Core.DTO.Health.Responses;
+using admin.Core.DTO.Versions.Responses;
 using admin.Core.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.Extensions.Logging;
@@ -8,10 +10,12 @@ namespace admin.Features.Versions;
 public partial class VersionsViewModel : BaseViewModel
 {
     public readonly List<string> Branches = new () { "Stable", "Dev" };
+    public List<AppVersionResponse>? Versions => AllVersions?.Versions[SelectedBranch];
 
     [ObservableProperty]
     private string _selectedBranch = "Stable";
 
+    private AllVersionsResponse? AllVersions;
     private readonly IApiService _apiService;
     private readonly ICurrentUserService _currentUserService;
     private readonly ILogger<VersionsViewModel> _logger;
@@ -25,5 +29,19 @@ public partial class VersionsViewModel : BaseViewModel
         _apiService = apiService;
         _currentUserService = currentUserService;
         _logger = logger;
+    }
+
+    partial void OnSelectedBranchChanged(string value)
+    {
+        OnPropertyChanged(nameof(Versions));
+    }
+
+    public async Task FetchAllVersions()
+    {
+        await HandleActionAsync(async () =>
+        {
+            AllVersions = await _apiService.Versions.GetAllVersions();
+            OnPropertyChanged(nameof(Versions));
+        });
     }
 }
