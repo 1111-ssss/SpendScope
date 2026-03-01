e122262c76e5512a552f4312573de4ffee252096
1111-ssss <gurskievgeni@gmail.com>
2026-03-01 12:31:27 +0300

    feat(versionsPage): add fields and remove command

--- admin/Features/Versions/VersionsViewModel.cs
+++ admin/Features/Versions/VersionsViewModel.cs
@@ -3,17 +3,28 @@ using admin.Core.DTO.Health.Responses;
 using admin.Core.DTO.Versions.Responses;
 using admin.Core.Interfaces;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.Extensions.Logging;
 
 namespace admin.Features.Versions;
 
 public partial class VersionsViewModel : BaseViewModel
 {
-    public readonly List<string> Branches = new () { "Stable", "Dev" };
+    public readonly List<string> Branches = new() { "Stable", "Dev" };
     public List<AppVersionResponse>? Versions => AllVersions?.Versions[SelectedBranch];
 
     [ObservableProperty]
     private string _selectedBranch = "Stable";
+
+    [ObservableProperty]
+    private AppVersionResponse? _selectedVersion;
+
+    public string? Build => SelectedVersion?.Build != null
+        ? $"Билд версии: {SelectedVersion?.Build.ToString()}"
+        : string.Empty;
+    public string? UploadedAt => SelectedVersion?.UploadedAt != null
+        ? $"Загружена: {SelectedVersion?.UploadedAt.Value.ToShortTimeString()}"
+        : string.Empty;
 
     private AllVersionsResponse? AllVersions;
     private readonly IApiService _apiService;
@@ -31,9 +42,22 @@ public partial class VersionsViewModel : BaseViewModel
         _logger = logger;
     }
 
+    public override void OnNavigatedTo()
+    {
+        base.OnNavigatedTo();
+
+        _ = FetchAllVersions();
+    }
+
     partial void OnSelectedBranchChanged(string value)
     {
         OnPropertyChanged(nameof(Versions));
+    }
+
+    partial void OnSelectedVersionChanged(AppVersionResponse? value)
+    {
+        OnPropertyChanged(nameof(Build));
+        OnPropertyChanged(nameof(UploadedAt));
     }
 
     public async Task FetchAllVersions()
@@ -42,6 +66,21 @@ public partial class VersionsViewModel : BaseViewModel
         {
             AllVersions = await _apiService.Versions.GetAllVersions();
             OnPropertyChanged(nameof(Versions));
+        });
+    }
+
+    [RelayCommand]
+    private async Task RemoveVersion()
+    {
+        await HandleActionAsync(async () =>
+        {
+            if (SelectedVersion?.Branch == null)
+                return;
+
+            await _apiService.Versions.DeleteVersion(SelectedVersion.Branch, SelectedVersion.Build.ToString());
+
+            OnPropertyChanged(nameof(Versions));
+            OnPropertyChanged(nameof(SelectedVersion));
         });
     }
 }
