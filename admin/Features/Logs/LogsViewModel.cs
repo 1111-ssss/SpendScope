e3574efbe5dd44156018ae50cb42a52fa5e53601
1111-ssss <gurskievgeni@gmail.com>
2026-03-07 08:05:18 +0300

    feat(logsPage): add fields

--- admin/Features/Logs/LogsViewModel.cs
+++ admin/Features/Logs/LogsViewModel.cs
@@ -1,10 +1,30 @@
 ﻿using admin.Core.Abstractions;
+using admin.Core.DTO.Logging.Responses;
 using admin.Core.Interfaces;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace admin.Features.Logs;
 
 public partial class LogsViewModel : BaseViewModel
 {
+    [ObservableProperty]
+    private LogListResponse? _logPage;
+
+    [ObservableProperty]
+    private int _currentPage = 1;
+
+    [ObservableProperty]
+    private int ? _pageSize = 10;
+
+    [ObservableProperty]
+    private LogResponse? _selectedLog;
+
+    public string CardText => SelectedLog != null
+        ? $"Уровень: {SelectedLog.Level}\nДата: {SelectedLog.Timestamp.ToShortDateString()}\nСообщение: {SelectedLog.Message}\n\nОшибка: {SelectedLog.Exception ?? "Отсутствует"}"
+        : "Здесь отобразится информация о журнале";
+
+    public List<LogResponse> LogPageItems => LogPage?.Items ?? new();
+
     private readonly IApiService _apiService;
     public LogsViewModel(
         IApiService apiService
